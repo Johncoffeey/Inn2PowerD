a0a35f14ed7c3e4a95e9a244e89b5411a5eb8e2b
Danni Dose <ddose@live.dk>
2017-11-07 10:52:16 +0100

    Added folders
    Hurray

--- src/inn2powerd/GUI/MainWindowController.java
+++ src/inn2powerd/GUI/MainWindowController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inn2powerd;
+package inn2powerd.GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
