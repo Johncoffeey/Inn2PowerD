27df4b0f35835cfbbaa3d85b9fc05f535a79ce84
Danni Dose <ddose@live.dk>
2017-11-09 14:35:32 +0100

    Added search func
    Malte version

--- src/inn2powerd/GUI/MainWindowController.java
+++ src/inn2powerd/GUI/MainWindowController.java
@@ -5,20 +5,32 @@
  */
 package inn2powerd.GUI;
 
+import be.Company;
 import dal.CompanyDAO;
+import inn2powerd.BLL.IWordSearcher;
+import inn2powerd.BLL.WordManager;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 /**
@@ -28,35 +40,41 @@ import javafx.scene.layout.Pane;
 public class MainWindowController implements Initializable
 {
 
+    private WordManager wordManager;
+
     private Label label;
     @FXML
     private Accordion tPanes;
 
 //    BLLManager bllManager = new BLLManager();
     @FXML
-    private AnchorPane tableCompanies;
+    private TextField txtQuery;
 
-    
+    FXMLLoader loader;
+    Pane infoWindow;
+    CompanyInfoWindowController CIWC;
+    CompanyDAO cDAO;
+    List<be.Company> list;
 
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
+        wordManager = new WordManager();
 
         try
         {
-            
-            CompanyDAO cDAO = new CompanyDAO();
-            List<be.Company> list = cDAO.getAllCompanies();
+
+            cDAO = new CompanyDAO();
+            list = cDAO.getAllCompanies();
+
             for (be.Company company : list)
             {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("CompanyInfoWindow.fxml"));
-                Pane infoWindow = loader.load();
-                CompanyInfoWindowController CIWC = loader.getController();
+                loader = new FXMLLoader(getClass().getResource("CompanyInfoWindow.fxml"));
+                infoWindow = loader.load();
+                CIWC = loader.getController();
                 CIWC.setCompany(company);
                 TitledPane pane = new TitledPane(company.getId() + "   -   " + company.getName(), infoWindow);
                 tPanes.getPanes().add(pane);
-                
-                System.out.println(company.getCountry());
             }
 
             //loadAllCompanies();
@@ -68,7 +86,63 @@ public class MainWindowController implements Initializable
 
     public void hashmap()
     {
-        
+
+    }
+
+    /**
+     * Performs a word search.
+     *
+     * @param event
+     */
+    @FXML
+    void handleSearch(ActionEvent event)
+    {
+        try
+        {
+            String query = txtQuery.getText().trim();
+            List<String> searchResult = null;
+            IWordSearcher searchStrategy = new IWordSearcher()
+            {
+                @Override
+                public boolean compare(String word)
+                {
+                    return true;
+                }
+            };
+
+            searchResult = wordManager.search(searchStrategy);
+
+            String limit = "10"; //comboLimitation.getValue();
+            try
+            {
+                int intLimit = Integer.parseInt(limit);
+                searchResult = searchResult.subList(0, intLimit);
+            } catch (NumberFormatException | IndexOutOfBoundsException nfe)
+            {
+                //Do nothing
+            }
+
+            tPanes.getPanes().clear();
+            List<Company> allCompanies = cDAO.getAllCompanies();
+            for (Company com : allCompanies)
+            {
+
+                if (com.getName().toLowerCase().contains(query.toLowerCase()))
+                {
+
+                    loader = new FXMLLoader(getClass().getResource("CompanyInfoWindow.fxml"));
+                    infoWindow = loader.load();
+                    CIWC = loader.getController();
+                    CIWC.setCompany(com);
+                    TitledPane pane = new TitledPane(com.getId() + "   -   " + com.getName(), infoWindow);
+                    tPanes.getPanes().add(pane);
+                }
+            }
+//            model.setWords(searchResult);
+        } catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
     }
 
 }
